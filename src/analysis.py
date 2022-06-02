d88ca14b379678902bcbed1e8809141126df278b
Vincent Leblond <vincent.leblond@gmail.com>
2022-06-02 23:23:08 +0200

    feat: export to geojson

--- src/analysis.py
+++ src/analysis.py
@@ -6,7 +6,13 @@ import geopandas as gpd
 import pandas as pd
 from geopy.distance import geodesic
 
-POIZ_JSON = "../data/web_site_export.json"
+# Set display options
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 100)
+pd.set_option("display.width", 1000)
+pd.set_option("mode.chained_assignment", None)
+
+POIZ_JSON = "../data/inputs/web_site_export.json"
 GARE_SNCF = (
     "https://ressources.data.sncf.com/api/v2/catalog/datasets/"
     + "referentiel-gares-voyageurs/exports/geojson?select=uic_code%2Cgare_alias_libelle_fronton%2Cwgs_84"
@@ -23,6 +29,7 @@ poiz_df = pd.DataFrame(poiz)
 poiz_df["key"] = 1
 poiz_df["lat"] = poiz_df.apply(lambda x: float(x["lat"]), axis=1)
 poiz_df["lng"] = poiz_df.apply(lambda x: float(x["lng"]), axis=1)
+# poiz_df = poiz_df[["nid", "title", "lat", "lng", "key"]]
 
 # %%
 # Read stations
@@ -31,6 +38,7 @@ stations["lat"] = stations["geometry"].y
 stations["lng"] = stations["geometry"].x
 stations = stations.query("lat==lat")
 stations["key"] = 1
+stations = stations[["uic_code", "gare_alias_libelle_fronton", "lat", "lng", "key"]]
 
 # %%
 # Compute distance
@@ -38,7 +46,7 @@ stations["key"] = 1
 
 def compute_distance(x):
     try:
-        distance = geodesic((x["lat"], x["lng"]), (x["lat_y"], x["lng_y"])).km
+        distance = geodesic((x["lat_x"], x["lng_x"]), (x["lat_y"], x["lng_y"])).km
     except:
         distance = 999999
     return distance
@@ -46,6 +54,18 @@ def compute_distance(x):
 
 df = poiz_df.merge(stations, how="left", on="key")
 df["distance"] = df.apply(lambda x: compute_distance(x), axis=1)
-# df = df.query("distance < 3")
 
+# %%
+# export to geojson
+table = (
+    df.query("distance < 1.5")
+    .groupby(["nid", "title", "lat_x", "lng_x"], as_index=False)
+    .first()
+)
+geotable = gpd.GeoDataFrame(
+    table, geometry=gpd.points_from_xy(table.lng_x, table.lat_x)
+)
+del geotable["field_departments_cities"]
+del geotable["field_quests"]
+geotable.to_file("../data/outputs/geotable.geojson", driver="GeoJSON")
 # %%
