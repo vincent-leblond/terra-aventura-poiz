38ff5c9b8950b2dec4c5d61e7148a9e525ce4066
Vincent Leblond <vincent.leblond@gmail.com>
2022-06-12 15:00:32 +0200

    wip

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # terra-aventura-poiz
+
 Analyse des poï'z de Terra Aventura
+
+## Configuration
+
+1. Installer Python
+2. Installer les librairies données dans *requirements.txt* : ```pip3 install -r requirements.txt```
+
+## Trouver les Poï'z à proximité des gares ferroviaires (moins de 1,5 km)
+
+Exécuter le script *src/analysis.py*
+
+```bash
+cd src
+python3 analysis.py
+```
+
+Le résultat est donné dans *data/outputs/geotable.geojson*
--- src/analysis.py
+++ src/analysis.py
@@ -1,10 +1,12 @@
 # %%
 # Initialisation
+import os
 import json
 import requests
 import geopandas as gpd
 import pandas as pd
 from geopy.distance import geodesic
+from bs4 import BeautifulSoup
 
 # Set display options
 pd.set_option("display.max_rows", 500)
@@ -12,24 +14,36 @@ pd.set_option("display.max_columns", 100)
 pd.set_option("display.width", 1000)
 pd.set_option("mode.chained_assignment", None)
 
-POIZ_JSON = "../data/inputs/web_site_export.json"
 GARE_SNCF = (
     "https://ressources.data.sncf.com/api/v2/catalog/datasets/"
-    + "referentiel-gares-voyageurs/exports/geojson?select=uic_code%2Cgare_alias_libelle_fronton%2Cwgs_84"
+    + "referentiel-gares-voyageurs/exports/geojson"
+    + "?select=uic_code%2Cgare_alias_libelle_fronton%2Cwgs_84"
     + "&limit=-1&timezone=UTC&pretty=false"
 )
+URL = "https://www.terra-aventura.fr/parcours"
+
+OUTPUT_DIR = "../data/outputs/"
+
+if not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
 
 # %%
-# Read data
-with open(POIZ_JSON, "r") as f:
-    data = json.load(f)
+# Read data from Terra Aventura web site
+html_text = requests.get(URL).text
+soup = BeautifulSoup(html_text, "html.parser")
+for script in soup.find_all("script"):
+    if script.get("data-drupal-selector") == "drupal-settings-json":
+        poiz_data = script.contents[0]
 
+data = json.loads(poiz_data)
+
+# %%
+# Read data json data
 poiz = data["geocaching_map"]["markers"]
 poiz_df = pd.DataFrame(poiz)
 poiz_df["key"] = 1
 poiz_df["lat"] = poiz_df.apply(lambda x: float(x["lat"]), axis=1)
 poiz_df["lng"] = poiz_df.apply(lambda x: float(x["lng"]), axis=1)
-# poiz_df = poiz_df[["nid", "title", "lat", "lng", "key"]]
 
 # %%
 # Read stations
@@ -41,7 +55,7 @@ stations["key"] = 1
 stations = stations[["uic_code", "gare_alias_libelle_fronton", "lat", "lng", "key"]]
 
 # %%
-# Compute distance
+# Compute distance to all stations
 
 
 def compute_distance(x):
@@ -53,10 +67,11 @@ def compute_distance(x):
 
 
 df = poiz_df.merge(stations, how="left", on="key")
+df = df.query("type=='geocaching_cache'")
 df["distance"] = df.apply(lambda x: compute_distance(x), axis=1)
 
 # %%
-# export to geojson
+# Filter data and export to geojson
 table = (
     df.query("distance < 1.5")
     .groupby(["nid", "title", "lat_x", "lng_x"], as_index=False)
@@ -67,5 +82,5 @@ geotable = gpd.GeoDataFrame(
 )
 del geotable["field_departments_cities"]
 del geotable["field_quests"]
-geotable.to_file("../data/outputs/geotable.geojson", driver="GeoJSON")
+geotable.to_file(OUTPUT_DIR + "geotable.geojson", driver="GeoJSON")
 # %%
